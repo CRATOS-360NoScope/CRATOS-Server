c468b98e956934b8af2d628c1c57aff6bce0f6a1
Christopher Wirt <cwirt@purdue.edu>
2015-10-07 20:24:49 +0000

    TurretController in working order, test driver in main.py

--- TurretController.py
+++ TurretController.py
@@ -1,11 +1,13 @@
 import RPi.GPIO as GPIO
-
+import time
 class TurretController:
 
 	GPIO_PIN_YAW   = 11 
 	GPIO_PIN_PITCH = 12
 	pwm_yaw   = None
 	pwm_pitch = None
+	current_pitch = 7.5
+	stopPitch = False
 	DEBUG = False
 
 	def __init__(self, pin_yaw, pin_pitch, debug=False):
@@ -17,6 +19,7 @@ class TurretController:
 		self.pwm_yaw = GPIO.PWM(self.GPIO_PIN_YAW, 53)
 		self.pwm_pitch = GPIO.PWM(self.GPIO_PIN_PITCH, 50)
 		self.pwm_yaw.start(7.5)
+		self.pwm_pitch.start(7.5)
 		self.DEBUG = debug		
 
 	def __del__(self):
@@ -25,14 +28,14 @@ class TurretController:
 		GPIO.cleanup()
 	
 	# direction +1 for clockwise, -1 for reverse
-	def startYaw(self, direction, fine=False):
-		modifier = 0.1 if fine else 2.5
+	def startYaw(self, direction, sensitivity=1):
+		modifier = 2.5*sensitivity
 		if self.DEBUG:
 			print "** startYaw **"
 			print "Direction: "+str(direction)
-			print "Fine: "+str(fine)
-			print "Duty Cycle: "+str(7.5-(modifier*direction))
-		self.pwm_yaw.ChangeDutyCycle(7.5-(modifier*direction))
+			print "Sensitivity: "+str(sensitivity)
+			print "Duty Cycle: "+str(7.5+(modifier*direction))
+		self.pwm_yaw.ChangeDutyCycle(7.5+(modifier*direction))
 
 	def stopYaw(self):
 		if self.DEBUG:
@@ -40,9 +43,27 @@ class TurretController:
 		self.pwm_yaw.ChangeDutyCycle(7.5)
 
 	# TODO: Auto stop for limit +60 or -30
-	def startPitch(self, direction, fine=False):
-		modifier = 10 if fine else 49
-                self.pwm_pitch.start(50-(modifier*direction))
+	def startPitch(self, direction, sensitivity=.1):
+		self.stopPitch = False
+		if self.DEBUG:
+			print "** startPitch **"
+		print "Direction: "+str(direction)
+		while (not self.stopPitch):
+			self.current_pitch += direction*sensitivity
+			if (self.current_pitch > 12.5):
+				self.current_pitch = 12.5
+				self.stopPitch = True
+				break
+			if (self.current_pitch < 0):
+				self.current_pitch = 0
+				self.stopPitch = True
+				break
+			print "Duty Cycle: "+str(self.current_pitch)
+			self.pwm_pitch.ChangeDutyCycle(self.current_pitch)
+			time.sleep(0.001)
+			
 
 	def stopPitch(self):
-		self.pwm_pitch.stop()
+		if self.DEBUG:
+			print "** stopPitch **"
+		self.stopPitch = True
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from TurretController import TurretController
+from subprocess import call
 import time
 
-ctrl = TurretController(pin_yaw=11, pin_pitch=12, debug=True)
+#call("hciconfig hci0 piscan")
 
+ctrl = TurretController(pin_yaw=11, pin_pitch=12, debug=True)
+time.sleep(2)
+
+ctrl.startPitch(1)
+print "Done 1"
+time.sleep(2)
+print "HI"
+ctrl.startPitch(-1)
+print "Done 2"
+time.sleep(2)
 ctrl.startYaw(1)
 
 time.sleep(2)
@@ -19,7 +30,7 @@ ctrl.stopYaw()
 
 time.sleep(2)
 
-ctrl.startYaw(1, True)
+ctrl.startYaw(1, .2)
 
 time.sleep(2)
 
@@ -27,7 +38,7 @@ ctrl.stopYaw()
 
 time.sleep(2)
 
-ctrl.startYaw(-1, True)
+ctrl.startYaw(-1, .2)
 
 time.sleep(2)
 
